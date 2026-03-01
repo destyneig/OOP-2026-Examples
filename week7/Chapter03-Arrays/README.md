8c74fe3b9ce0ca966f4d193670df9249bc450763
Jeremy Callinan <jeremy.callinan@gmail.com>
2026-03-01 13:26:57 -0500

    Add week7 chapter 3 and 4 JavaScript DS&A code examples

---
+++ week7/Chapter03-Arrays/README.md
@@ -0,0 +1,3 @@
+# Chapter 3 - Arrays
+
+Code examples for Chapter 3 (Arrays) from *Learning JavaScript Data Structures and Algorithms (4th Edition)*.
---
+++ week7/Chapter04-Stacks/03-decimal-to-binary.js
@@ -0,0 +1,49 @@
+// Decimal to binary conversion using a stack
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.items = {};
+  }
+
+  push(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+}
+
+function decimalToBinary(decNumber) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let rem;
+  let binaryString = '';
+
+  while (number > 0) {
+    rem = Math.floor(number % 2);
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+
+  return binaryString;
+}
+
+console.log(decimalToBinary(233));
+console.log(decimalToBinary(10));
+console.log(decimalToBinary(1000));
